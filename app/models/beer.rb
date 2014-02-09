b6a00d7a5a0cf48f0bf4f470b4061dcf3ec53c42
Jouni <jjvaris@gmail.com>
2014-02-09 23:51:12 +0200

    Viikko 4

--- app/models/beer.rb
+++ app/models/beer.rb
@@ -6,9 +6,11 @@ class Beer < ActiveRecord::Base
   has_many :raters, -> { uniq }, through: :ratings, source: :user
 
   validates :name, presence: true
+  validates :style, presence: true
 
 
   def to_s
     return name+" - "+brewery.name
   end
+
 end
